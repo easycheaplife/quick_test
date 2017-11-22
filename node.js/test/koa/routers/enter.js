10a4eae566e42218746b5e28afc043c6b9632df8
King Lee <lijin157@163.com>
2017-11-22 15:05:04 +0800

    add axios test

--- node.js/test/koa/routers/enter.js
+++ node.js/test/koa/routers/enter.js
@@ -1,8 +1,9 @@
 const Router = require('koa-router');
-const test = require('./test');
+const test = require('../controller/testController');
 const router = new Router();
 
 router.get('/test',test.test);
 router.get('/test1',test.test1);
+router.get('/test2',test.test2);
 
 module.exports = router;
--- node.js/test/koa/util/logger.js
+++ node.js/test/koa/util/logger.js
@@ -1,7 +1,7 @@
 const log4js = require('log4js');
 log4js.configure(require('../config/log4js'));
-var logger_access = log4js.getLogger('access');
 
 module.exports = {
-	access:logger_access
+	access:log4js.getLogger('access'),
+	api:log4js.getLogger('api')
 }
